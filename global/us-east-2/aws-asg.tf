778b4051ac19c92402fe7de2ecceb2a565433d1b
Dev <owlman1911@gmail.com>
2020-05-24 16:34:45 -0400

    created autoscaling groups

---
+++ global/us-east-2/aws-asg.tf
@@ -0,0 +1,38 @@
+resource "aws_launch_configuration" "lconf_web" {
+    image_id    =   "ami-0323c3dd2da7fb37d"
+    instance_type = "t2.micro"
+    security_groups = [aws_security_group.sg-web.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.web-srv-prt} &
+                EOF
+    
+    lifecycle {
+        create_before_destroy   = true
+    }
+        
+}
+
+resource "aws_autoscaling_group" "asg-web" {
+    launch_configuration = aws_launch_configuration.lconf_web.name
+    vpc_zone_identifier  = data.aws_subnet_ids.df-sub.ids
+
+    min_size = 2
+    max_size = 4
+
+    tag {
+        key     = "Name"
+        value   = "tf-asg-web"
+        propagate_at_launch = true
+    }
+}
+
+data "aws_vpc" "df-vpc" {
+    default = true
+}
+
+data "aws_subnet_ids" "df-sub" {
+    vpc_id = data.aws_vpc.df-vpc.id
+}
