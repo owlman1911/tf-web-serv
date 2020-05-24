e996dd25577f9a6b098efdb3960d9a3b7dbab53e
dev <dev@The-Wall.local>
2020-05-24 01:32:29 -0400

    added tag to web server

--- global/us-east-2/main.tf
+++ global/us-east-2/main.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "webserv" {
     ami         = "ami-0323c3dd2da7fb37d"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "web-app",
+        Environment = "test"
+    }
 }
