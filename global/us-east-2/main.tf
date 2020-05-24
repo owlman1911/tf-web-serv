31048ac9ec25859a30488443c5b962bf16d7cb02
dev <dev@The-Wall.local>
2020-05-24 02:30:05 -0400

    updating user_data again

--- global/us-east-2/main.tf
+++ global/us-east-2/main.tf
@@ -1,17 +1,17 @@
 resource "aws_instance" "webserv" {
-    ami         = "ami-0323c3dd2da7fb37d"
-    instance_type = "t2.micro"
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "web-app",
-        Environment = "test"
-    }
+  tags = {
+    Name        = "web-app",
+    Environment = "test"
+  }
 
-    user_data = <<- EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
 
- 
+
 }
